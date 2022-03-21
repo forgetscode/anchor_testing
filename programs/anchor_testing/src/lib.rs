d46414b6699509a90e4375150393f86b9cdcd7a1
michael <forgetscode@gmail.com>
2022-03-21 00:32:16 -0700

    fee testing

--- programs/anchor_testing/src/lib.rs
+++ programs/anchor_testing/src/lib.rs
@@ -5,10 +5,61 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod anchor_testing {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
         Ok(())
     }
 }
 
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(text: String)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = MyAccount::space(&text))]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl MyAccount {
+    fn space(text: &str) -> usize {
+        // discriminator
+        8 +
+        // u8 + u16 + u32 + u64 + u128
+        1 + 2 + 4 + 8 + 16 +
+        // i8 + i16 + i32 + i64 + i128
+        1 + 2 + 4 + 8 + 16 +
+        // bool + char
+        1 + 4 +
+        // String
+        4 + text.len()
+    }
+}
+
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Delete<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub data: String,
+}
+
--- tests/anchor_testing.ts
+++ tests/anchor_testing.ts
@@ -2,6 +2,7 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { AnchorTesting } from '../target/types/anchor_testing';
 
+
 describe('anchor_testing', () => {
 
   // Configure the client to use the local cluster.
@@ -9,9 +10,54 @@ describe('anchor_testing', () => {
 
   const program = anchor.workspace.AnchorTesting as Program<AnchorTesting>;
 
+  const user= program.provider.wallet;
+
+  const myAccount = anchor.web3.Keypair.generate();
+
+
   it('Is initialized!', async () => {
+
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+
+    let _balance = await program.provider.connection.getBalance(user.publicKey);
+
+    let test_length = "hello my name is fredrick, I am from norway! I am going to write a long essay for you to demonstrate my exceptional capabilities as a writer. I am the best writer in the world as you can tell which is why I have so much to say! It is truely apparent from the musings of my brain that my writing has characteristics that far surpass the average genius!";
+
+    const tx = await program.rpc.initialize(test_length, {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      signers: [myAccount],
+    });
+
+    let _balanceInit = await program.provider.connection.getBalance(user.publicKey);
+    console.log("cost of initialization: $", (_balance* (10**-9) - _balanceInit* (10**-9))*90);
+
+    let _myAccountData = await program.account.myAccount.fetch(myAccount.publicKey);
+    console.log(_myAccountData);
+
     console.log("Your transaction signature", tx);
+
+    let _myAccount = await program.account.myAccount.fetch(myAccount.publicKey);
+    console.log(_myAccount.data);
+
+    let test_longer_length = "trivial change"
+
+    const tx2 = await program.rpc.update(test_longer_length, {
+      accounts: {
+        myAccount: myAccount.publicKey,
+      }
+    });
+
+    let _balanceUpdate = await program.provider.connection.getBalance(user.publicKey);
+    console.log("cost of update: $", (_balanceInit* (10**-9) - _balanceUpdate* (10**-9))*90);
+
+    let _myAccountDataNew = await program.account.myAccount.fetch(myAccount.publicKey);
+    console.log(_myAccountDataNew);
+
+    console.log("Your transaction signature", tx2);
+
   });
 });
